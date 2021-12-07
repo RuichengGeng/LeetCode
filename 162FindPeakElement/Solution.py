f9add1bc1324e9a6b9b575774c5ee446f48e1d8d
GengRuicheng <ruicheng.geng@hotmail.com>
2021-12-07 21:10:30 +0800

    Create Solution.py

---
+++ 162FindPeakElement/Solution.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  7 17:11:12 2021
+
+@author: Ruich
+"""
+
+
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        
+        while left < right:
+            mid = int((left + right) / 2)
+            if nums[mid] > nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+        return left
